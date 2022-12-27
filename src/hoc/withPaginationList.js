47b7d73883d9a61dda992ffc04f527661290b3c6
NaufalAulia <naufal7600@gmail.com>
2022-12-27 11:11:10 +0700

    Add higher-order component for list components

---
+++ src/hoc/withPaginationList.js
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
+import { PaginationControl, StyledContainer } from "../components";
+
+const Empty = () => <h1>No Item</h1>;
+
+export default function withPaginationList(ListComponent, options) {
+  return (props) => {
+    const { onGetData, label } = options;
+
+    const { onNavigate } = props;
+
+    const [isLoading, setLoading] = useState(false);
+
+    const [page, setPage] = useState(0);
+    const [size, setSize] = useState(2);
+    const [data, setData] = useState([]);
+    const [count, setCount] = useState(0);
+    const [totalPages, setTotalPages] = useState(0);
+    const [totalCount, setTotalCount] = useState(0);
+
+    useEffect(() => {
+      setLoading(true);
+      onGetData(page, size).then((pagedData) => {
+        setData(pagedData.data);
+        setCount(pagedData.count);
+        setTotalPages(pagedData.totalPage);
+        setTotalCount(pagedData.totalCount);
+        setLoading(false);
+      });
+    }, [onGetData, page, size]);
+
+    return (
+      <StyledContainer className="d-flex flex-column align-items-stretch gap-4">
+        <h1>{label} List</h1>
+        <Button
+          variant="success"
+          onClick={() => onNavigate(`add-${label.toLowerCase()}`)}
+          disabled={isLoading}
+          className="align-self-start"
+        >
+          Add Course
+        </Button>
+        {data.length === 0 ? (
+          <Empty />
+        ) : (
+          <>
+            <ListComponent data={data} />
+            <PaginationControl
+              page={page}
+              size={size}
+              count={count}
+              totalPages={totalPages}
+              totalCount={totalCount}
+              onChangePage={setPage}
+              onChangeSize={setSize}
+              disabled={isLoading}
+            />
+          </>
+        )}
+      </StyledContainer>
+    );
+  };
+}
--- src/pages/CourseList/CourseList.jsx
+++ src/pages/CourseList/CourseList.jsx
@@ -4,8 +4,7 @@ import { ListGroup } from "react-bootstrap";
 import { PaginationControl, StyledContainer } from "../../components";
 import CourseItem from "./components/CourseItem";
 import services from "../../services";
-
-const Empty = () => <h1>No Item</h1>;
+import withPaginationList from "../../hoc/withPaginationList";
 
 const List = (props) => {
   const { data } = props;
@@ -19,59 +18,9 @@ const List = (props) => {
   );
 };
 
-const CourseList = (props) => {
-  const { onNavigate } = props;
-
-  const [isLoading, setLoading] = useState(false);
-
-  const [page, setPage] = useState(0);
-  const [size, setSize] = useState(2);
-  const [courses, setCourses] = useState([]);
-  const [count, setCount] = useState(0);
-  const [totalPages, setTotalPages] = useState(0);
-  const [totalCount, setTotalCount] = useState(0);
-
-  useEffect(() => {
-    setLoading(true);
-    services.course.getCourses(page, size).then((pagedCourses) => {
-      setCourses(pagedCourses.data);
-      setCount(pagedCourses.count);
-      setTotalPages(pagedCourses.totalPage);
-      setTotalCount(pagedCourses.totalCount);
-      setLoading(false);
-    });
-  }, [page, size]);
-
-  return (
-    <StyledContainer className="d-flex flex-column align-items-stretch gap-4">
-      <h1>Course List</h1>
-      <Button
-        variant="success"
-        onClick={() => onNavigate("add-course")}
-        disabled={isLoading}
-        className="align-self-start"
-      >
-        Add Course
-      </Button>
-      {courses.length === 0 ? (
-        <Empty />
-      ) : (
-        <>
-          <List data={courses} />
-          <PaginationControl
-            page={page}
-            size={size}
-            count={count}
-            totalPages={totalPages}
-            totalCount={totalCount}
-            onChangePage={setPage}
-            onChangeSize={setSize}
-            disabled={isLoading}
-          />
-        </>
-      )}
-    </StyledContainer>
-  );
-};
+const CourseList = withPaginationList(List, {
+  onGetData: services.course.getCourses,
+  label: "Course",
+});
 
 export default CourseList;
